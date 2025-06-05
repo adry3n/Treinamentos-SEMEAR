9437c3eec8e5ca160692ce3226dae9526d52eb31
adry3n <adryenmendes2016@gmail.com>
2025-06-05 19:54:45 -0300

    configuração do codigo gpio para piscar um led

--- Esp32-Testes/src/main.c
+++ Esp32-Testes/src/main.c
@@ -1,1 +1,21 @@
-void app_main() {}
+#include <freertos/FreeRTOS.h>
+#include "freertos/task.h"
+#include "driver/gpio.h"
+
+#define PINO_LED GPIO_NUM_2
+
+
+void app_main() {
+    // Configurar o GPIO como saída
+    gpio_set_direction(PINO_LED, GPIO_MODE_OUTPUT);
+
+    while (1) {
+        // Ligar o LED
+        gpio_set_level(PINO_LED, 1);
+        vTaskDelay(1000 / portTICK_PERIOD_MS); // Espera 500ms
+
+        // Desligar o LED
+        gpio_set_level(PINO_LED, 0);
+        vTaskDelay(1000 / portTICK_PERIOD_MS); // Espera 500ms
+    }
+}
